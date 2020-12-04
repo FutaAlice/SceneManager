dd588881427e3fe671b8a968bdf8c4da92ebc4c7
xb.wang <futaalice@emails.bjut.edu.cn>
2020-12-04 14:57:09 +0800

    牛逼

--- Source/SceneManager/Private/EventHub.cpp
+++ Source/SceneManager/Private/EventHub.cpp
@@ -6,6 +6,8 @@
 #include "Components/SceneComponent.h"  // USceneComponent
 #include "Engine/Light.h"   // ALight
 #include "Components/LightComponent.h"  // ULightComponent
+#include "Misc/CoreDelegates.h" // FCoreDelegates
+#include "UObject/UObjectGlobals.h" // FCoreUObjectDelegates
 
 std::shared_ptr<EventHub> EventHub::_instance;
 std::mutex EventHub::_mutex;
@@ -37,53 +39,22 @@ EventHub::EventHub()
         UE_LOG(LogTemp, Warning, TEXT("OnDuplicateActorsEnd"));
     });
 
-    /*
-    FPropertyEditorModule& PropertyEditor = FModuleManager::Get().LoadModuleChecked<FPropertyEditorModule>("PropertyEditor");
-    static TSharedPtr<IPropertyChangeListener> PropertyChangeListener = PropertyEditor.CreatePropertyChangeListener();
-    PropertyChangeListener->GetOnPropertyChangedDelegate().AddLambda(
-        [](const TArray<UObject*>& ChangedObjects, const IPropertyHandle& PropertyHandle) {
-            UE_LOG(LogTemp, Warning, TEXT("PropertyChangeListener"));
-        });
-
-    FPropertyListenerSettings Settings;
-    Settings.bIgnoreArrayProperties = false;
-    Settings.bIgnoreObjectProperties = false;
-    // Property flags which must be on the property
-    Settings.RequiredPropertyFlags = CPF_Edit; // or CPF_Edit
-    // Property flags which cannot be on the property
-    Settings.DisallowedPropertyFlags = CPF_None;
-
-
-    // Select (or SelectObjectEvent/SelectNoneEvent)
-    static AActor *SelectedActor = nullptr;
-    USelection::SelectionChangedEvent.AddLambda([&](UObject *Object) {
-        AActor* OldSelectedActor = SelectedActor;
-        SelectedActor = GEditor->GetSelectedActors()->GetBottom<AActor>();
-
-        //if (USelection* Selection = Cast<USelection>(Object)) {
-        //    for (int32 Idx = 0; Idx < Selection->Num(); Idx++) {
-        //        UObject *SelectedObject = Selection->GetSelectedObject(Idx);
-        //        UE_LOG(LogTemp, Warning, TEXT("SelectionChangedEvent: %s"), *SelectedObject->GetName());
-        //    }
-
-        //}
-
-        if (SelectedActor && SelectedActor != OldSelectedActor) {
-            UE_LOG(LogTemp, Warning, TEXT("SelectionChangedEvent: %s"), *SelectedActor->GetName());
-            if (ALight *Light = Cast<ALight>(SelectedActor)) {
-                UE_LOG(LogTemp, Warning, TEXT("Light: %s"), *Light->GetName());
-                PropertyChangeListener->SetObject(*Light->GetLightComponent(), Settings);
-                PropertyChangeListener->TriggerAllPropertiesChangedDelegate();
-
-            }
-        }
+    // RENAME
+    FCoreDelegates::OnActorLabelChanged.AddLambda([](AActor *Actor) {
+        UE_LOG(LogTemp, Warning, TEXT("OnActorLabelChanged"));
     });
-    */
+
+    // MODIFY
+    FCoreUObjectDelegates::OnObjectPropertyChanged.AddLambda([](UObject*, struct FPropertyChangedEvent &) {
+        UE_LOG(LogTemp, Warning, TEXT("OnObjectPropertyChanged"));
+    });
+
 
     //FEditorDelegates::MapChange.AddLambda([]() {
     //    UE_LOG(LogTemp, Warning, TEXT("MapChange"));
     //});
 
+    // NOT WORK
     FEditorDelegates::ActorPropertiesChange.AddLambda([]() {
         UE_LOG(LogTemp, Warning, TEXT("ActorPropertiesChange"));
     });
